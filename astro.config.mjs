881e0fd21c06d2062e9d0e9c64bc99b16203bdf0
sokolawesome <sokolawesome@gmail.com>
2025-04-26 03:01:15 +0300

    fix(design): Tailwind styles and Catppuccin theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
-  site: "https://sokolawesome.github.io",
-  base: "/",
+  output: 'static',
+  site: 'https://sokolawesome.github.io',
+  base: '/',
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+    },
   },
   plugins: [],
 };
